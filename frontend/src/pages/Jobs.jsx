3f40aedd310174109e6ce7f8b889ce6f0fb57250
Ayush Kumar <149200631+siayush735@users.noreply.github.com>
2026-04-29 13:01:10 +0530

    Update Jobs.jsx

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -8,7 +8,7 @@ export default function Jobs() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/api/jobs")
+      .get("https://recruitment-app-cz6l.onrender.com/api/jobs")
       .then((res) => setJobs(res.data))
       .catch((err) => console.error(err))
       .finally(() => setLoading(false));
